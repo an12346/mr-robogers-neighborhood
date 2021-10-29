29c81e179c7695e07a65645625b818376f83e03a
Anna Pittman <annaepittman@gmail.com>
2021-10-29 08:38:38 -0700

    create test 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Specs
+Describe: robo()
+Test: "It should return an array with a 1 if the number 1 is inputted."
+Code: robo(1)
+Expected output: 1
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//$(document).ready(function(){
+  //let roboArray = []
+
+//})
+
+//Test 1:
+
+function populateList(inroboArray) {
+  let counter = 0;
+  let roboArray = [];
+  let robo = [1];
+
+}
